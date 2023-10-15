e0f078f07d14409a0208dcc8823f79c565d890a3
Max <profex55@gmail.com>
2023-10-15 14:18:15 -0500

    init commit

---
+++ tcp_server.py
@@ -0,0 +1,28 @@
+import socket, threading
+
+IP= '127.0.0.1'
+PORT = 9999
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP,PORT))
+    server.listen(5)
+    print(f'[*] Listening on {IP}:{PORT}')
+
+    while True:
+        client, address = server.accept()
+        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
+        # server.close()
+
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Recived: {request.decode()}')
+        sock.send(b'ACK')
+        
+        
+
+if __name__ == '__main__':
+    main()
